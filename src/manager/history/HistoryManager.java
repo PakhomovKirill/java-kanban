7e9b9b48142470871408b5ed6f4cc98e913bd8ae
pakhomov.kirill <kirill.pakhomov.stm.labs@yandex.ru>
2024-03-27 01:07:03 +0300

    sprint_8 update

--- src/manager/history/HistoryManager.java
+++ src/manager/history/HistoryManager.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface HistoryManager<T> {
 
    public void addTaskToHistory(T task);
-   
+
    public void removeTaskFromHistory(Integer id);
 
    public ArrayList<T> getTasksHistory();
