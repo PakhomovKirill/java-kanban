750a5810054160d1c2e7ad294ea896773855c48a
pakhomov.kirill <kirill.pakhomov.stm.labs@yandex.ru>
2024-03-27 01:01:55 +0300

    sprint_8 update

--- src/manager/history/InMemoryHistoryManager.java
+++ src/manager/history/InMemoryHistoryManager.java
@@ -4,14 +4,14 @@ import task.Task;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class InMemoryHistoryManager<T extends Task> implements HistoryManager<T>{
+public class InMemoryHistoryManager<T extends Task> implements HistoryManager<T> {
     HashMap<Integer, Node> nodeList = new HashMap<>();
     DoubleLinkedList linkedList = new DoubleLinkedList();
 
-    public void addTaskToHistory (T task){
+    public void addTaskToHistory (T task) {
         int taskId = task.getId();
 
-        if(this.nodeList.containsKey(taskId)){
+        if (this.nodeList.containsKey(taskId)) {
             removeTaskFromHistory(taskId);
         }
 
@@ -19,12 +19,12 @@ public class InMemoryHistoryManager<T extends Task> implements HistoryManager<T>
         this.nodeList.put(taskId, addNode);
     }
 
-    public void removeTaskFromHistory(Integer taskId){
+    public void removeTaskFromHistory(Integer taskId) {
         Node removedNode = this.nodeList.remove(taskId);
         this.linkedList.removeFromList(removedNode);
     }
 
-    public ArrayList<T> getTasksHistory(){
+    public ArrayList<T> getTasksHistory() {
         return this.linkedList.getList();
     }
 }
