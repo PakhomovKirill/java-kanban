518f6eae822d3ff1d5c99a469a6f9591972745d0
pakhomov.kirill <kirill.pakhomov.stm.labs@yandex.ru>
2024-03-27 00:13:31 +0300

    sprint_8 update

--- src/task/Epic.java
+++ src/task/Epic.java
@@ -12,28 +12,28 @@ public class Epic extends Task {
     setEndStartTime();
   }
 
-  public Epic(String title, String description, Integer taskId){
+  public Epic(String title, String description, Integer taskId) {
     super(title, description, Enums.TaskStatus.NEW, taskId);
     super.setTaskType(Enums.TasksType.EPIC);
 
     setEndStartTime();
   }
 
-  private void setEndStartTime(){
+  private void setEndStartTime() {
     TreeMap<Long, Task> start = new TreeMap<>();
     TreeMap<Long,Task> end = new TreeMap<>();
 
-    if(this.childList.size() == 0){
+    if (this.childList.size() == 0) {
       return;
     }
 
     this.childList.values().stream().forEach(item -> {
       Long time = item.getStartTimeToSeconds();
-      if(time != null){
+
+      if (time != null) {
         start.put(time, item);
         end.put(time, item);
       }
-
     });
 
     ArrayList<Task> list = new ArrayList<>(end.values());
@@ -43,18 +43,18 @@ public class Epic extends Task {
 
   }
 
-  public void setStatus(Enums.TaskStatus status){
+  public void setStatus(Enums.TaskStatus status) {
     super.setStatus(status);
   }
 
-  public void setChildSubtask(Subtask subtask){
+  public void setChildSubtask(Subtask subtask) {
 
     this.childList.put(subtask.getId(), subtask);
 
     setEndStartTime();
   }
 
-  public void removeChildSubtask(int subtaskId){
+  public void removeChildSubtask(int subtaskId) {
     this.childList.remove(subtaskId);
   }
 
@@ -62,25 +62,17 @@ public class Epic extends Task {
     return this.childList;
   }
 
-  public void updateChildList(int subtaskId, Subtask subtask){
+  public void updateChildList(int subtaskId, Subtask subtask) {
     this.childList.put(subtaskId, subtask);
   }
 
-  public void clearChildSubtasks(){
+  public void clearChildSubtasks() {
     this.childList.clear();
   }
 
   @Override
   public String toString() {
-    String result =
-            this.getId()+
-            ","+ this.getTaskType() +
-            "," + this.getTitle()+
-            "," + this.getStatus() +
-            "," + this.getDescription() +
-            ",null" +
-            "," + this.getStartTimeFormatted() +
-            "," + this.getEndTimeFormatted();
+    String result = this.getId() + ","+ this.getTaskType() + "," + this.getTitle() + "," + this.getStatus() + "," + this.getDescription() + ",null" + "," + this.getStartTimeFormatted() + "," + this.getEndTimeFormatted();
 
     return result + "," + this.getAllChildrenList().toString() + "";
   }
